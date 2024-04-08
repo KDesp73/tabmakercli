53362084d991bd2b6cfab7629314f1da3619d0b9
konLiogka <ntinaliogka@gmail.com>
2024-04-08 22:42:14 +0300

    Multiple single notes on single string

--- tabmakercli.c
+++ tabmakercli.c
@@ -10,71 +10,41 @@
 #include <ctype.h>
 
 // HEADERS 
-//
-// Define constants for tuning names and strings
+
 #define NUM_TUNINGS 8
 int xpos=5;
 int ypos=1;
+int tabNum=1;
 int rows, cols;
 
-char *tuningNames[NUM_TUNINGS] = {"E", 
-                                  "D#",
-                                  "D", 
-                                  "C#", 
-                                  "C",  
-                                  "B", 
-                                  "A#",
-                                  "A"};
-
+char *tuningNames[NUM_TUNINGS] =
+  {
+   "E", 
+   "D#",
+   "D", 
+   "C#", 
+   "C",  
+   "B", 
+   "A#",
+   "A"
+  };
+// Add more tunings as needded, dont forget to also add the name and change NUM_TUNINGS
 char *tuningStrings[NUM_TUNINGS][6] = {
     // Standard Tunings
     {"E", "B", "G", "D", "A", "E"}, // E Standard
-    {"D#", "A#", "F#", "C#", "G#", "D#"}, // D# Standard / Eb Standard
+    {"D#", "A#", "F#", "C#", "G#", "D#"}, // D# Standard 
     {"D", "A", "F", "C", "G", "D"}, // D Standard
-    {"C#", "G#", "F#", "C#", "G#", "C#"}, // C# Standard / Db Standard
-    {"C", "G", "E", "B", "F#", "C"}, // C Standard / B Standard
+    {"C#", "G#", "F#", "C#", "G#", "C#"}, // C# Standard 
+    {"C", "G", "E", "B", "F#", "C"}, // C Standard 
     {"B", "F#", "D#", "A#", "F", "B"}, // B Standard
-    {"A#", "F", "D", "A", "E", "A#"}, // A# Standard / Bb Standard
+    {"A#", "F", "D", "A", "E", "A#"}, // A# Standard 
     {"A", "E", "C#", "G#", "D#", "A"} // A Standard
 };
 
 
 
 // MAIN PROGRAM
-void setTuning(char *input){
-  // Extract tuning specifier from input
-        char tuningSpec[256];
-        strcpy(tuningSpec, input + 2); // Skip "t " prefix
-        int tuningIndex = -1;
- 
-        // Determine the new tuning index based on the input tuning specifier
-        // Modify this part according to your tuning specifications
-        for (int i =0; i<NUM_TUNINGS;++i)
-        {
-          if(strcasecmp(tuningSpec, tuningNames[i]) == 0)
-          {
-            tuningIndex = i;
-            break;
-          }
-        }
-         if(tuningIndex==-1) {
-            // Add cases for other tunings as needed
-            mvprintw(LINES - 2, 0, "Invalid tuning specifier"); // Notify user of invalid input
-            refresh();
-            return;
-        }
 
-               // Draw guitar tuning
-    for (int i = 2; i < 8; i++) {
-        mvprintw(i, 1, " ");
-        mvprintw(i, 0, "%s", tuningStrings[tuningIndex][i-2]);
-    }
-  refresh();
-
-         
-      
-
-}
 
 void movePos()
 {
@@ -83,64 +53,105 @@ void movePos()
   {
     xpos=1;
     ypos+=8;
+    tabNum++;
   }
 }
 
-// Add a note/s
-void addNote(char input[])
-{
-   // Move to position
-   mvprintw(input[0]-'0'+ypos,xpos,"%c", input[2]);
-   movePos();
-   refresh();
+void setTuning(char *input){
+ 
+    char tuningSpec[256];
+    strcpy(tuningSpec, input + 2);
+    int tuningIndex = -1;
+    
+    for (int i =0; i<NUM_TUNINGS;++i)
+    {
+      if(strcasecmp(tuningSpec, tuningNames[i]) == 0)
+      {
+        tuningIndex = i;
+        break;
+      }
+    }
+    if(tuningIndex==-1)
+    {
+      mvprintw(LINES - 2, 0, "Invalid tuning specifier"); 
+      refresh();
+      return;
+    }
    
+    for (int i = 2; i < 8; i++) 
+    {
+      mvprintw(i, 1, " ");
+      mvprintw(i, 0, "%s", tuningStrings[tuningIndex][i-2]);
+    }
+    refresh();
 }
 
-void handleInput(WINDOW *input_win) {
+
+void addNote(char input[])
+{
+   char notes[32];
+   strcpy(notes, input+2);
+   if(strlen(notes)==1)
+   {
+     mvprintw(input[0]-'0'+ypos,xpos,"%c", notes[0]);
+     movePos();
+     refresh();
+
+   }else
+   {
+     for(int i=0;i<strlen(notes);i+=2)
+     {
+        mvprintw(input[0]-'0'+ypos,xpos,"%c", notes[i]);
+      movePos();
+      refresh();
+     }
+   }
+}
+
+void handleInput(WINDOW *input_win)
+{
     char input[16];
     int input_len = 0;
-    // Display input prompt
     mvwprintw(input_win, 0, 0, "input command: ");
-    wrefresh(input_win); // Refresh input window to make prompt visible
-
-    // Enable cursor visibility
+    wrefresh(input_win); 
     curs_set(1);
 
-    while (1) {
-        int ch = wgetch(input_win); // Get character input
-        if (ch == '\n') {
-            // Clear input line
+    while (1) 
+        {
+        int ch = wgetch(input_win); 
+        if (ch == '\n') 
+        {
+            
             wmove(input_win, 0, 14);
             wclrtoeol(input_win);
             wrefresh(input_win);
-            break; // Exit loop on Enter key
-        } else if (ch == 127 || ch == KEY_BACKSPACE) { // Handle backspace/delete key
-            if (input_len > 0) {
-                // Move cursor one position back
+            break; 
+        } else if (ch == 127 || ch == KEY_BACKSPACE) 
+        {
+            if (input_len > 0) 
+            {
+                
                 mvwaddch(input_win, 0, 14 + input_len - 1, ' ');
                 wmove(input_win, 0, 14 + input_len - 1);
                 wrefresh(input_win);
-                input[--input_len] = '\0'; // Remove character from input
+                input[--input_len] = '\0'; 
             }
-        } else if (input_len < sizeof(input) - 1) { // Add character to input
+        } else if (input_len < sizeof(input) - 1) 
+        { 
             input[input_len++] = ch;
-            input[input_len] = '\0'; // Null-terminate input string
+            input[input_len] = '\0'; 
 
-            // Display character at cursor position
+            
             mvwaddch(input_win, 0, 14 + input_len - 1, ch);
             wrefresh(input_win);
         }
-    }
-
-    // Disable cursor visibility
+    } 
     curs_set(0);
-
-    // Convert input string to lowercase
-    for (int i = 0; input[i]; i++) {
+    for (int i = 0; input[i]; i++) 
+    {
         input[i] = tolower(input[i]);
     }
 
-    // Check for tuning command
     if (strncmp(input, "t ", 2) == 0)
     {
       setTuning(input);
@@ -153,22 +164,22 @@ void handleInput(WINDOW *input_win) {
     }
 }
 
-int main() {
-    // Initialize ncurses
+int main() 
+{
+    
     initscr();
     cbreak();
     noecho();
     scrollok(stdscr,TRUE);
-    // Create window for input
+    
     WINDOW *input_win = newwin(1, COLS, LINES - 1, 0);
     scrollok(input_win, FALSE);
 
-    // Display help message
     mvprintw(LINES - 2, 0, "(? for help)");
-    getmaxyx(stdscr, rows, cols); // Get the dimensions of the terminal screen
-
-   // Draw guitar tabs for E Standard
-    for (int i = 2; i < 8; i++) {
+    getmaxyx(stdscr, rows, cols); 
+   
+    for (int i = 2; i < 8; i++) 
+    {
        mvprintw(i,1, "%s %s", tuningStrings[0][i-2],"|");
        for(int j=4;j<cols;j++)
        {
@@ -176,13 +187,12 @@ int main() {
        }
     }
 
-    // Refresh the screen
-    refresh();    // Main loop to handle user input
-    while (1) {
+    refresh();   
+    while (1) 
+    {
         handleInput(input_win);
     }
 
-    // End ncurses
     endwin();
 
     return 0;
