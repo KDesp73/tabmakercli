ea4de88b9da84cab3c7d7eb3599311e5acb6c4d1
konLiogka <ntinaliogka@gmail.com>
2024-04-21 15:05:12 +0300

    Add: Delete, Chords, 2D Char Array

--- tabmakercli.c
+++ tabmakercli.c
@@ -11,45 +11,32 @@
  *  Add support for more strings
  *  Add Tempo, comments, Parts
  *  Export as txt
- *  Add chords
- *  Delete chords/notes
  *  Delete Tabs
  *  Copy/Paste whole Tabs
  */
-
-
-// Honestly this code is a mess, good luck figuring it out.
-// It does what it needs to do.
 
 #include <ncurses.h>
 #include <string.h>
 #include <ctype.h>
 
 
-// HEADERS
-
-// Init number of tunings, time signatures,
-// strings and gap for comments (tempo, song part etc.)
-
 #define NUM_TUNINGS 8  
 #define NUM_TIMESIG 25
 #define NUM_STRINGS 6
+#define NUM_ARGS 10 
+#define LEN_ARGS 10
 #define COM_GAP 3
 
-// Initial y,p position, current tab,
-// rows and columns and number of tabs
 int xpos = 7;
 int ypos = COM_GAP;
 int curTab = 1;
 int rows, cols;
 int numTabs = 1;
 
-// Tuning names 
+
 char *tuningNames[NUM_TUNINGS] =
     {"E", "D#", "D", "C#", "C", "B", "A#", "A"};
-// Add more tunings as needded, dont forget to also add the name and change NUM_TUNINGS
 char *tuningStrings[NUM_TUNINGS][NUM_STRINGS] = {
-    // Standard Tunings
     {"E", "B", "G", "D", "A", "E"},       // E Standard
     {"D#", "A#", "F#", "C#", "G#", "D#"}, // D# Standard
     {"D", "A", "F", "C", "G", "D"},       // D Standard
@@ -60,7 +47,7 @@ char *tuningStrings[NUM_TUNINGS][NUM_STRINGS] = {
     {"A", "E", "C#", "G#", "D#", "A"}     // A Standard
 };
 
-// Time signatures, add more as needed but change size
+
 char *timeSignatures[NUM_TIMESIG] =
     {
         "4/4", "10/8", "2/4", "6/8", "9/8",
@@ -70,26 +57,18 @@ char *timeSignatures[NUM_TIMESIG] =
         "5/16", "13/8", "10/8", "15/8", "16/8"};
 
 
-// Allowed characters, anotations (used inbetween
-// the allowed chars) 
-char *allowedChars = " 1234567890bx~";
-
-char *allowedAno = "ph/'\\'";
 
 
-// PROGRAM
-
-// Showing current position of indicator
 void showPos()
 {
   curs_set(0);
-  move(ypos + NUM_STRINGS, 0); // Clear indicator
+  move(ypos + NUM_STRINGS, 0); 
   clrtoeol();
-  mvprintw(ypos + NUM_STRINGS, xpos, "^"); // Redraw with current xpos
+  mvprintw(ypos + NUM_STRINGS, xpos, "^"); 
   refresh();
 }
 
-// Render the lines for every string
+
 void renderTab()
 {
   for (int i = ypos; i < ypos + NUM_STRINGS; i++)
@@ -102,88 +81,84 @@ void renderTab()
   numTabs++;
 }
 
-// Move the Y position
+
 void moveYPos(int y)
 {
   
-  if (y == -1) // Go up a tab
+  if (y == -1) 
   {
-    if (curTab>1 && xpos>=6) // Avoid going to tunings
+    if (curTab>1 && xpos>=6)
     {
-      move(ypos + NUM_STRINGS, 0); // Clear indicator
+      move(ypos + NUM_STRINGS, 0); 
       clrtoeol();
-      ypos -= COM_GAP + NUM_STRINGS; // Go up
+      ypos -= COM_GAP + NUM_STRINGS; 
       curTab--;
       showPos();
     }
   }
-  else if (y == 1 ) // Go down a tab
+  else if (y == 1 )
   {
-      move(ypos + NUM_STRINGS, 0);   // Clear indicator
+      move(ypos + NUM_STRINGS, 0);  
       clrtoeol();
-      ypos += COM_GAP + NUM_STRINGS; // Go down 
+      ypos += COM_GAP + NUM_STRINGS; 
       curTab++;
       showPos();
   }
 }
 
-// Move the X position
 void moveXPos(int x)
 {
-  move(ypos + NUM_STRINGS, 0);  // Clear indicator
+  move(ypos + NUM_STRINGS, 0);  
   clrtoeol();
-  // If xpos doesnt interferre with tunings 
+ 
   if (xpos+x > 4 || (xpos+x>0 && curTab>1)) 
   {
-    xpos += x;  // Move xpos
-    if (xpos >= cols) // If xpos at the end
+    xpos += x; 
+    if (xpos >= cols)
     {
-      xpos = 2;       // Move to the beginning  
-      if(numTabs==curTab) // If tabs don't exist, add
+      xpos = 2;      
+      if(numTabs==curTab) 
       {
         moveYPos(1);
         renderTab();
       }
       else if(numTabs>curTab)
       {
-        moveYPos(1);   // else just move
+        moveYPos(1);  
       }
     }
     showPos();
   }
-  else if (curTab>1) // If current tab isnt 1
+  else if (curTab>1)
   {
-    xpos = cols - 1; // Go back to end of cols of prev
+    xpos = cols - 1; 
     moveYPos(-1);
     showPos();
   }
 }
 
-// Set tuning depending on tuning names
-void setTuning(char *input)
+void setTuning(char tuning[][LEN_ARGS])
 {
 
-  char tuningSpec[16];
-  strcpy(tuningSpec, input + 2);
   int tuningIndex = -1;
-  // Iterate over every tuning
+
   for (int i = 0; i < NUM_TUNINGS; ++i)
   {
-    // If input matches tuning names, set tuning index
-    if (strcasecmp(tuningSpec, tuningNames[i]) == 0)
+   
+    if (strcasecmp(tuning[1], tuningNames[i]) == 0)
     {
       tuningIndex = i;
       break;
     }
   }
-  // If there's no matching name, give error
+
   if (tuningIndex == -1)
   {
-    mvprintw(LINES - 2, 0, "Invalid tuning");
+    mvprintw(LINES - 2, 0, "No tuning found: %s", tuning[1]);
     refresh();
     return;
   }
-  // Change tuning
+  
   for (int i = 0; i < NUM_STRINGS; i++)
   {
     mvprintw(COM_GAP + i, 2, " ");
@@ -192,107 +167,89 @@ void setTuning(char *input)
   refresh();
 }
 
-// Add single note or multiple notes or notes with anotations
-void addNote(char input[])
-{
-  char notes[32];
-  strcpy(notes, input + 2);
-  int selectedString = input[0] - '0' - 1;
-  // Iterate over the input, minus command
-  for (int i = 0; i < strlen(notes); i++)
+
+void addNote(char notes[][LEN_ARGS])
+{ 
+  int selectedString = notes[0][0] - '0' - 1;
+  for(int i=1; i<NUM_ARGS; i++)
   {
-    // If current note isn't space and an anotation
-    if (notes[i] != ' ' && strchr(allowedAno, notes[i]) == NULL)
+    if(strcmp(notes[i],"")==0)
     {
-
-      // If next note exists, is not space, is allowed char and current is allowed char
-      // example: 12
-      if ((i + 1) < strlen(notes) && 
-           notes[i + 1] != ' ' && 
-           strchr(allowedChars, notes[i + 1]) != NULL &&
-           strchr(allowedChars, notes[i]) != NULL)
-      {
-        moveXPos(1);
-        mvprintw(selectedString + ypos, xpos - 1, "%c", notes[i]);
-        mvprintw(selectedString + ypos, xpos, "%c", notes[i + 1]);
-        i++;
-        moveXPos(2);
-      }
-      // Else if next char is allowed anotation and second next is allowed char
-      // Example: 2h3
-      else if (strchr(allowedAno, notes[i + 1]) != NULL &&
-               (i + 2) < strlen(notes) &&
-               notes[i + 2] != ' ' &&
-               strchr(allowedChars,notes[i+2]!=NULL)
-      {
-        moveXPos(2);
-        mvprintw(selectedString + ypos, xpos - 2, "%c", notes[i]);
-        mvprintw(selectedString + ypos, xpos - 1, "%c", notes[i + 1]);
-        mvprintw(selectedString + ypos, xpos, "%c", notes[i + 2]);
-
-        i += 2;
-        moveXPos(2);
-      }
-      // Else if its just a single allowed char 
-      else if ((strchr(allowedChars, notes[i]) != NULL))
-      {
-        mvprintw(selectedString + ypos, xpos, "%c", notes[i]);
-        moveXPos(2);
-      }
+     break; 
+    }
+    else
+    {                 
+      mvprintw(selectedString + ypos,xpos,"%s", notes[i]);
+      moveXPos(strlen(notes[i])+1);
     }
   }
 }
 
-
-// Add a time signature
-void addTimeSignature(char input[])
+void addTimeSignature(char* timeSig)
 {
   bool flag = false;
-  // Check time signature list
-  for (int j = 0; j < NUM_TIMESIG; j++)
+ 
+  for (int i = 0; i < NUM_TIMESIG; i++)
   {
-    // If input is found in list, flag = true
-    if (strcmp(input + 2, timeSignatures[j]) == 0)
+    
+    if (strcmp(timeSig, timeSignatures[i]) == 0)
     {
       flag = true;
       break;
     }
   }
-  if (flag) // If flag is true
+  if (flag)
   {
-    int line = ypos + 2; 
-    // Iterate over the input, skip first two chars
-    for (int j = 2; j < strlen(input); j++)
+    int line = ypos + 2;
+    int column = xpos;
+    
+    for (int i = 0; i < strlen(timeSig); i++) 
     {
-      // If char is /, print and move next line and skip 
-      if (input[j] == '/')
-      {
-        column = xpos;
+    
+      if (timeSig[i] == '/')
+      { 
         line++;
         mvprintw(line, column, "/");
         line++;
-        j++;
+        i++;
       }
-      // Print the numbers, can be singleor double digits
-      mvprintw(line, column, "%c", input[j]);
-      column++;
+      
+      mvprintw(line, column, "%c", timeSig[i]);
+     
     }
     moveXPos(3);
   }
 }
 
 
-// Handle input from input_win
+void addChord(char chords[][LEN_ARGS])
+{
+ for(int i=1;i<NUM_ARGS;i++)
+  {
+    if(strcmp(chords[i],"")==0)
+    {
+      break;
+    }
+    else 
+    {
+      for(int j=0; j<NUM_STRINGS; j++)
+      {
+        mvprintw(j+ypos,xpos,"%c", chords[i][j]);
+        
+      }
+      moveXPos(2);
+    }
+  }
+}
 void handleInput(WINDOW *input_win)
 {
-  char input[32];
+  char input[NUM_ARGS * LEN_ARGS];
   int input_len = 0;
   mvwprintw(input_win, 0, 0, "input command: ");
   wrefresh(input_win);
-
   while (1)
   {
-    int ch = wgetch(input_win); // Get characters
+    int ch = wgetch(input_win); 
     if (ch == '\n')
     {
       wmove(input_win, 0, 14);
@@ -300,7 +257,7 @@ void handleInput(WINDOW *input_win)
       wrefresh(input_win);
       break;
     }
-    else if (ch == 127 || ch == KEY_BACKSPACE) // If backspace, clear everything
+    else if (ch == KEY_BACKSPACE)
     {
       if (input_len > 0)
       {
@@ -310,7 +267,7 @@ void handleInput(WINDOW *input_win)
         input[--input_len] = '\0';
       }
     }
-    // Use arrow keys or keypad to move indicator
+ 
     else if (ch == KEY_RIGHT)
     {
       moveXPos(1);
@@ -324,14 +281,23 @@ void handleInput(WINDOW *input_win)
       if(numTabs!=curTab)
       {
         moveYPos(1);
-      }
-     
+      }  
     }
     else if (ch == KEY_UP)
     {
       moveYPos(-1);
     }
-    else if (input_len < sizeof(input) - 1) // Add input to input_win
+    else if ( ch == KEY_DC)
+    {
+        moveXPos(-1);
+        for (int j = 0; j < NUM_STRINGS; j++)
+        {
+          mvprintw(j + ypos, xpos, "-");
+        }
+
+
+    }
+    else if (input_len < sizeof(input) - 1) 
     {
       curs_set(1);
       input[input_len++] = ch;
@@ -339,38 +305,72 @@ void handleInput(WINDOW *input_win)
 
       mvwaddch(input_win, 0, 14 + input_len - 1, ch);
       wrefresh(input_win);
+      move(LINES-2, 0); 
+      clrtoeol();
+      mvprintw(LINES - 2, 0, "(? for help)");
+  
+   
+
     }
   }
-  // Case insensitive
-  for (int i = 0; input[i]; i++)
+   if(strlen(input)>1)
   {
-    input[i] = tolower(input[i]);
+      char output[NUM_ARGS][LEN_ARGS];
+      int pos=0;
+      char *pch;
+      pch = strtok(input, " ");
+
+      for (int i = 0; i < NUM_ARGS; i++) 
+      {
+        for (int j = 0; j < LEN_ARGS; j++) 
+        {
+            output[i][j] = '\0';         
+        }
+      }
+
+          
+      while (pch != NULL) {
+          
+            strcpy(output[pos], pch+'\0');
+            pos ++;
+            pch = strtok(NULL, " ");
+
+        }
+     
+           
+
+      if (strcmp(output[0], "t") == 0)
+      {
+        setTuning(output);
+      }     
+      else if (isdigit(output[0][0]) && output[0][0] - '0' <= NUM_STRINGS && output[0][1]== '\0')
+      {
+       
+       addNote(output);
+      }
+      else if (strcmp(output[0], "s") == 0)
+      {
+        addTimeSignature(output[1]);
+      }
+      else if (strcmp(output[0], "c") == 0)
+      {
+       addChord(output);
+      }
+      else if (strcmp(input, "|") == 0)
+      {
+        for (int j = 0; j < NUM_STRINGS; j++)
+        {
+          mvprintw(j + ypos, xpos, "|");
+        }
+        moveXPos(2);
+      }
+
   }
-  // CCheck for commands
-  if (strncmp(input, "t ", 2) == 0)
-  {
-    setTuning(input);
-  }
-  else if (isdigit(input[0]) && input[0] < '7')
-  {
-    addNote(input);
-  }
-  else if (strncmp(input, "s ", 2) == 0)
-  {
-    addTimeSignature(input);
-  }
-  else if (strcmp(input, "|") == 0)
-  {
-    for (int j = 0; j < 6; j++)
-    {
-      mvprintw(j + ypos, xpos, "|");
-    }
-    moveXPos(2);
-  }
+   
   refresh();
 }
 
-// MAIN
+
 int main()
 {
   initscr();
@@ -382,7 +382,7 @@ int main()
   WINDOW *input_win = newwin(1, COLS, LINES - 1, 0);
   scrollok(input_win, FALSE);
   keypad(input_win, TRUE);
-
+ 
   mvprintw(LINES - 2, 0, "(? for help)");
   getmaxyx(stdscr, rows, cols);
   for (int i = ypos; i < COM_GAP + NUM_STRINGS; i++)
@@ -398,10 +398,8 @@ int main()
   refresh();
   while (1)
   {
-
     handleInput(input_win);
   }
-
-endwin();
+  endwin();
   return 0;
 }
